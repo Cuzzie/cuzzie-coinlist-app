ec376aaefbc3b0da8032da54bc6ba4d99bafc1e1
Kelwin Lee <kelwin.lee@gmail.com>
2017-06-18 18:14:12 +0800

    First commit

---
+++ src/main/java/org/cuzzie/coinlist/CoinlistApplication.java
@@ -0,0 +1,12 @@
+package org.cuzzie.coinlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoinlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoinlistApplication.class, args);
+	}
+}
